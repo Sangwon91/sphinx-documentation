e654613e61306e787b3ecec7a7e1cc7590396aed
Sangwon <lsw91.main@gmail.com>
2022-12-31 17:34:41 +0900

    Theme to furo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -47,7 +47,7 @@ exclude_patterns = []
 #
 # html_theme = 'sphinx_rtd_theme'
 # html_theme = 'sphinx_material'
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'furo'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
